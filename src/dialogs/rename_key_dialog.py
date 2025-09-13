bb5f263f48b763880f920652f2145b4404b1d218
komed3 <pk97@outlook.de>
2025-09-13 19:55:55 +0200

    Create rename_key_dialog.py

---
+++ src/dialogs/rename_key_dialog.py
@@ -0,0 +1,60 @@
+"""
+TranslateHub - Rename Key Dialog
+Dialog for renaming translation keys
+"""
+
+from typing import Optional
+
+from PyQt6.QtWidgets import QDialog, QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QWidget
+
+from ..helpers import ok_close
+
+
+class RenameKeyDialog ( QDialog ) :
+    """Dialog for renaming translation keys"""
+
+    def __init__ ( self, parent: Optional[ QWidget ] = None, old_key: str = "" ) :
+        """
+        Initialize rename translation key dialog
+        Args:
+            parent: Parent widget
+            old_key: Current key name
+        """
+
+        super().__init__( parent )
+        self.setWindowTitle( "Rename Translation Key" )
+        self.resize( 400, 120 )
+
+        self.layout: QVBoxLayout = QVBoxLayout()
+
+        # Old key display
+        old_key_layout = QHBoxLayout()
+        self.old_key_label = QLabel( "Current Key:" )
+        self.old_key_display = QLineEdit( old_key )
+        self.old_key_display.setReadOnly( True )
+
+        old_key_layout.addWidget( self.old_key_label )
+        old_key_layout.addWidget( self.old_key_display, 1 )
+
+        self.layout.addLayout( old_key_layout )
+
+        # New key input
+        new_key_layout = QHBoxLayout()
+        self.new_key_label = QLabel( "New Key:" )
+        self.new_key_input = QLineEdit()
+
+        new_key_layout.addWidget( self.new_key_label )
+        new_key_layout.addWidget( self.new_key_input, 1 )
+
+        self.layout.addLayout( new_key_layout )
+
+        # Dialog buttons
+        self.layout.addWidget( ok_close( self.accept, self.reject ) )
+
+        self.setLayout( self.layout )
+
+
+    def get_new_key ( self ) -> str :
+        """Get the entered new key"""
+
+        return self.new_key_input.text()
