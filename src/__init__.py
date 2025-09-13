a6dcb633594a4098646cd666af5f6deb668abf28
komed3 <pk97@outlook.de>
2025-09-14 00:41:50 +0200

    Create src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+TranslateHub - Translation Management Tool
+Cross-platform translation management tool for i18n projects
+"""
+
+from .main import VERSION
+from .run import main
+
+__name__ = "TranslateHub"
+__version__ = VERSION
+__all__ = [ "main" ]
